8e40e2740b49b72b26f93aeadf33229945885e78
SantiagoSauma <lssaumap@gmail.com>
2024-08-20 07:35:30 +0200

    Chat Engine Function Added

--- src/App.js
+++ src/App.js
@@ -1,3 +1,16 @@
 import { ChatEngine } from "react-chat-engine"
 
 import './App.css'
+
+const App = () => {
+    return (
+        <ChatEngine 
+            height = "100vh"
+            projectID = "20167b8a-062c-48b4-9e11-d8b66d7b9948"
+            userName = "dummy"
+            userSecret = "Pepelarana"
+        />
+    )
+}
+
+export default App;
