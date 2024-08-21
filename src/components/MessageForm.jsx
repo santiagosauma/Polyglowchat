86c76dc520fdd70acb9aa1ef4026f75b2fa59702
SantiagoSauma <lssaumap@gmail.com>
2024-08-21 07:07:21 -0600

    Sending Messages now possible

--- src/components/MessageForm.jsx
+++ src/components/MessageForm.jsx
@@ -1,12 +1,19 @@
-import {useState} from 'react'
-const MessageForm = () => {
+import { useState } from 'react'
+import { sendMessage, isTyping } from 'react-chat-engine'
+const MessageForm = (props) => {
     const [value, setValue] = useState('')
-    const handleSubmit = () => {
-        
+    const { chatId, creds } = props
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        const text = value.trim();
+        if(text.length > 0) {
+            sendMessage(creds, chatId, { text })
+        }
     }
 
-    const handleChange = () => {
-    
+    const handleChange = (event) => {
+        setValue(event.target.value)
+        isTyping(props, chatId)
     }
 
     return(
